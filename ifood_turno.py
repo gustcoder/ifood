2b2930d70a8ef43e00a1b4de1ab31ea0e0de6123
gustcoder <gustavofaramos@gmail.com>
2024-11-03 23:14:27 -0300

    Melhorias e add de regras

--- ifood_turno.py
+++ ifood_turno.py
@@ -1,5 +1,6 @@
 import json
 import random
+import sys
 import os
 os.system("")
 
@@ -11,30 +12,43 @@ COLOR = {
     "ENDC": "\033[0m",
 }
 
+#  o arquivo ifood_turno simula uma base de dados
 with open('ifood_turno.json', 'r') as arquivo:
-    motoboys = json.load(arquivo)  # carrega o conteúdo do arquivo em um dicionário Python
+    motoboys = json.load(arquivo)
 
 
 def obter_qtd_motoboys(horario_demanda, pedidos_demanda):
     if len(motoboys) == 0:
         return pedidos_demanda // utr_pedidos
     else:
-        motoboys_disponiveis = 0
+        motoboys_disponiveis = []
+        qtd_motoboys_disponiveis = 0
+        #  varre nossa base de dados em busca de motoboys cadastrados dentro do período informado
         for motoboy in motoboys:
             if horario_demanda >= motoboy['hora_inicio_turno'] and horario_demanda <= motoboy['hora_fim_turno']:
-                motoboys_disponiveis += 1
+                qtd_motoboys_disponiveis += 1
+                motoboys_disponiveis.append(motoboy)
 
-        if motoboys_disponiveis > 0:
-            return motoboys_disponiveis
+        if qtd_motoboys_disponiveis > 0:
+            print(f"Já existe(m) " + str(qtd_motoboys_disponiveis) + " motoboy(s) cadastrados que contemplam este período. Lista: ")
+            for motoboy in motoboys_disponiveis:
+                print(COLOR["BLUE"], f"Nome: {motoboy["nome"]} | Início Turno: {motoboy["hora_inicio_turno"]}h | Fim Turno: {motoboy["hora_fim_turno"]}h", COLOR["ENDC"])
+
+            resposta = input("Deseja continuar? (s/n): ")
+            if resposta.lower() == 'n': 
+                sys.exit()
+            else:
+                return qtd_motoboys_disponiveis
         else:
             return pedidos_demanda // utr_pedidos
 
 
-def add_motoboy(nome, hora_inicio, fim_turno):
+def add_motoboy(nome, hora_inicio, tempo_turno, pedidos_demanda):
     motoboy = {
         "nome": nome,
         "hora_inicio_turno": hora_inicio,
-        "hora_fim_turno": (hora_inicio + fim_turno) - 1
+        "hora_fim_turno": (hora_inicio + tempo_turno) - 1,
+        "qtd_pedidos": pedidos_demanda
     }
     motoboys.append(motoboy)
     with open('ifood_turno.json', 'w') as turnos:
@@ -47,10 +61,10 @@ if __name__ == "__main__":
 
     qtd_motoboys = obter_qtd_motoboys(horario_demanda, pedidos_demanda)
 
-    print(COLOR["BLUE"], "Você irá precisar de " + str(qtd_motoboys) + " motoboy(s)!", COLOR["ENDC"])
+    print(COLOR["BLUE"], f"Você irá precisar de " + str(qtd_motoboys) + " motoboy(s)!", COLOR["ENDC"])
 
     for i in range(qtd_motoboys):
         codigo_motoboy = random.randint(1, 100)
         tempo_turno = int(input("Quanto tempo de turno para o motoboy " + str(i+1) + "?  "))
-        add_motoboy("Motoboy IFOOD-00" + str(codigo_motoboy), horario_demanda, tempo_turno)
-        print(COLOR["GREEN"], "Motoboy cadastrado!", motoboys, COLOR["ENDC"])
+        add_motoboy("Motoboy IFOOD-00" + str(codigo_motoboy), horario_demanda, tempo_turno, pedidos_demanda)
+        print(COLOR["GREEN"], f"Motoboy cadastrado!", motoboys, COLOR["ENDC"])
